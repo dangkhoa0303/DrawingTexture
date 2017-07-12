378d54c67ac5b4a6adfd8f37c8a4e2451e33071b
Khoa Phung <khoa2.phung@live.uwe.ac.uk>
2017-07-12 22:18:11 +0700

    Tried to create another shader program, but failed

--- app/src/main/java/com/example/dangkhoa/drawingtexture/DirectVideo.java
+++ app/src/main/java/com/example/dangkhoa/drawingtexture/DirectVideo.java
@@ -42,35 +42,71 @@ public class DirectVideo {
                     "void main() {" +
                     "  vec4 camera = texture2D(s_texture, textureCoordinate);" +
                     "  vec4 img = texture2D(img_texture, textureCoordinate);" +
-                    "  gl_FragColor = vec4(mix(camera.rgb, img.rgb, img.a*1.0), camera.a);" +
+                    //"  gl_FragColor = vec4(mix(camera.rgb, img.rgb, img.a*1.0), camera.a);" +
+                    "  gl_FragColor = camera;" +
                     "}";
+
+    private String vs_Image = "attribute vec2 a_texCoord;"
+            + "varying vec2 v_texCoord;"
+            + "attribute vec4 a_Position;"
+            + "void main() {"
+            + "  gl_Position = a_Position;"
+            + "  v_texCoord = a_texCoord;"
+            + "}";
+
+    private String fs_Image = "varying vec2 v_texCoord;"
+            + "uniform sampler2D s_texture;"
+            + "void main() {"
+            + "  gl_FragColor = texture2D(s_texture, v_texCoord);"
+            + "}";
 
     private FloatBuffer vertexBuffer, textureVerticesBuffer;
     private ShortBuffer drawListBuffer;
 
+    // variables used for camera preview texture
     private final int mProgram;
     private int mPositionHandle;
     private int mCameraTextureHandle;
-    private int mImageTextureHandle;
     private int mTextureCoordHandle;
+
+    // variables used for image texture
+    private final int mImageProgram;
+    private int mImagePositionHandle;
+    private int mImageTextureHandle;
+    private int mImageTextureCoordHandle;
+
+    private FloatBuffer imageVertexBuffer;
 
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 2;
 
     static float squareVertices[] = {
+            1.0f, -1.0f,
+            1.0f, 1.0f,
+            -1.0f, 1.0f,
+            1.0f, -1.0f,
             -1.0f, 1.0f,
             -1.0f, -1.0f,
-            1.0f, -1.0f,
-            1.0f, 1.0f
+    };
+
+    static float imageSquareVertices[] = {
+            0.5f, -0.5f,
+            0.5f, 0.5f,
+            -0.5f, 0.5f,
+            0.5f, -0.5f,
+            -0.5f, 0.5f,
+            -0.5f, -0.5f
     };
 
     // in counterclockwise order:
     float textureVertices[] = {
+            1.0f, 1.0f,
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 1.0f,
             0.0f, 0.0f,
             1.0f, 0.0f,
-            1.0f, 1.0f,
-            0.0f, 1.0f
     };
 
     private short drawOrder[] = {
@@ -91,6 +127,12 @@ public class DirectVideo {
         vertexBuffer.put(squareVertices);
         vertexBuffer.position(0);
 
+        ByteBuffer bb1 = ByteBuffer.allocateDirect(imageSquareVertices.length * 4);
+        bb1.order(ByteOrder.nativeOrder());
+        imageVertexBuffer = bb1.asFloatBuffer();
+        imageVertexBuffer.put(imageSquareVertices);
+        imageVertexBuffer.position(0);
+
         ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
         dlb.order(ByteOrder.nativeOrder());
         drawListBuffer = dlb.asShortBuffer();
@@ -110,6 +152,14 @@ public class DirectVideo {
         GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
         GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
         GLES20.glLinkProgram(mProgram);
+
+        int img_vertexShader = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER, vs_Image);
+        int img_fragmentShader = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER, fs_Image);
+
+        mImageProgram = GLES20.glCreateProgram();             // create empty OpenGL ES Program
+        GLES20.glAttachShader(mImageProgram, img_vertexShader);   // add the vertex shader to program
+        GLES20.glAttachShader(mImageProgram, img_fragmentShader); // add the fragment shader to program
+        GLES20.glLinkProgram(mImageProgram);
     }
 
     private void loadImageTexture() {
@@ -146,16 +196,36 @@ public class DirectVideo {
         mCameraTextureHandle = GLES20.glGetUniformLocation(mProgram, "s_texture");
         GLES20.glUniform1i(mCameraTextureHandle, 0);
 
-        mImageTextureHandle = GLES20.glGetUniformLocation(mProgram, "img_texture");
-        GLES20.glUniform1i(mImageTextureHandle, 1);
+        //mImageTextureHandle = GLES20.glGetUniformLocation(mProgram, "img_texture");
+        //GLES20.glUniform1i(mImageTextureHandle, 1);
 
-        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+        //GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
         GLES20.glDisableVertexAttribArray(mTextureCoordHandle);
 
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        //GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+        //GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+    }
+
+    public void drawImage() {
+        GLES20.glUseProgram(mImageProgram);
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
+
+        mImagePositionHandle = GLES20.glGetAttribLocation(mImageProgram, "a_Position");
+        GLES20.glEnableVertexAttribArray(mImagePositionHandle);
+        GLES20.glVertexAttribPointer(mImagePositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, imageVertexBuffer);
+
+        mImageTextureCoordHandle = GLES20.glGetAttribLocation(mImageProgram, "a_texCoord");
+        GLES20.glEnableVertexAttribArray(mImageTextureCoordHandle);
+        GLES20.glVertexAttribPointer(mImageTextureCoordHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, textureVerticesBuffer);
+
+        mImageTextureHandle = GLES20.glGetUniformLocation(mImageProgram, "s_texture");
+        GLES20.glUniform1i(mImageTextureHandle, 1);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
     }
 }
